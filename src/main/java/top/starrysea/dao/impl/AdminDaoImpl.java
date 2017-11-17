6e3ce94bca1d867df80f06aefa3052448b3d956c
liuyang <liuyang@Lenovo-PC>
2017-11-17 20:12:19 +0800

    实现了IAdminDao中的全部方法

--- src/main/java/top/starrysea/dao/impl/AdminDaoImpl.java
+++ src/main/java/top/starrysea/dao/impl/AdminDaoImpl.java
@@ -1,16 +1,40 @@
 package top.starrysea.dao.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import top.starrysea.common.DaoResult;
 import top.starrysea.dao.IAdminDao;
 import top.starrysea.entity.Admin;
 
+import static top.starrysea.common.Common.isNotNull;
+import static top.starrysea.common.Common.md5;
+
+@Repository("adminDao")
 public class AdminDaoImpl implements IAdminDao {
+	@Autowired
+	private JdbcTemplate template;
 
 	@Override
 	// 管理员登陆
 	public DaoResult loginDao(Admin admin) {
-		// TODO 自动生成的方法存根
-		return null;
+		String sql = "SELECT 1 " + "FROM admin " + "WHERE admin_useraccount = ?";
+		List<String> list = template.queryForList(sql, new Object[] { admin.getAdminUseraccount() }, String.class);
+		if (list.size() == 0) {
+			return new DaoResult(false, "管理员账号不存在");
+		}
+		sql = "SELECT admin_id " + "FROM admin " + "WHERE admin_useraccount = ? " + "AND admin_password = ?";
+		List<Admin> theResult = template.query(sql,
+				new Object[] { admin.getAdminUseraccount(), md5(admin.getAdminPassword()) },
+				(rs, row) -> new Admin.Builder().adminId(rs.getInt("admin_id")).build());
+		if (isNotNull(theResult)) {
+			return new DaoResult(true, theResult.get(0));
+		} else {
+			return new DaoResult(false, "密码错误");
+		}
 	}
 
 }
