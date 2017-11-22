84a7fa8cd5c9eeff99b0ab9987c3be31773e8d64
liuyang <liuyang@windows10.microdone.cn>
2017-11-23 00:58:15 +0800

    增加对addWorkService的测试方法

--- src/test/java/top/starrysea/service/WorkServiceTest.java
+++ src/test/java/top/starrysea/service/WorkServiceTest.java
@@ -1,12 +1,20 @@
 package top.starrysea.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import top.starrysea.StarrtseaApplication;
+import top.starrysea.common.Common;
 import top.starrysea.common.Condition;
 import top.starrysea.entity.Work;
 import top.starrysea.service.IWorkService;
@@ -29,4 +37,13 @@ public class WorkServiceTest {
 	public void queryWorkService() {
 		System.out.println(workService.queryWorkService(new Work.Builder().workId(6).build()));
 	}
+	@Test
+	public void addWorkService(){
+		try {
+			MockMultipartFile file=new MockMultipartFile("5.pdf", new FileInputStream(new File("F://jetty-distribution-9.4.7.v20170914.zip")));
+			System.out.println(workService.addWorkService(file, new Work.Builder().workName("a").workStock(5).workUploadTime(Common.getNowDate()).build()));
+		}catch (Exception e) {
+			System.out.println("错错错");
+		}
+		}
 }
