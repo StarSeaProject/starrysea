131f2a17c76b95a332124b13f077d2cee4e49345
xiongjunlang <xiongjunlang@agree.com.cn>
2018-01-04 19:28:01 +0800

    增加两个test

---
+++ src/test/java/top/starrtsea/dao/ActivityImageDaoTest.java
@@ -0,0 +1,27 @@
+package top.starrtsea.dao;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import top.starrysea.StarrtseaApplication;
+import top.starrysea.dao.IActivityImageDao;
+import top.starrysea.object.dto.Activity;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StarrtseaApplication.class)
+public class ActivityImageDaoTest {
+
+	@Autowired
+	private IActivityImageDao dao;
+	
+	@Test
+	public void getAllActivityImageDao() {
+		System.out.println(dao.getAllActivityImageDao(new Activity.Builder().build()).getResult(List.class));
+	}
+
+}
---
+++ src/test/java/top/starrtsea/dao/OnlineDaoTest.java
@@ -0,0 +1,32 @@
+package top.starrtsea.dao;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import top.starrysea.StarrtseaApplication;
+import top.starrysea.common.Common;
+import top.starrysea.dao.IOnlineDao;
+import top.starrysea.object.dto.Online;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StarrtseaApplication.class)
+public class OnlineDaoTest {
+
+	@Autowired
+	private IOnlineDao dao;
+	
+	@Test
+	public void saveOnlineDao() {
+		System.out.println(dao.saveOnlineDao(new Online.Builder().onlineId(Common.getCharId("O-", 10)).onlineEmail("111").build()).getErrInfo());
+	}
+
+	@Test
+	public void getAllOnlineDao() {
+		System.out.println(dao.getAllOnlineDao().getResult(List.class));
+	}
+}
