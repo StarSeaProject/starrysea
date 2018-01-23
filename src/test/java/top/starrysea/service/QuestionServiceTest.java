65c6970b4227bb9f24c2243910003e0883be4746
liuyang <liuyang@windows10.microdone.cn>
2018-01-24 00:46:22 +0800

    增加QuestionService的测试类

---
+++ src/test/java/top/starrysea/service/QuestionServiceTest.java
@@ -0,0 +1,38 @@
+package top.starrysea.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import top.starrysea.StarrtseaApplication;
+import top.starrysea.common.Condition;
+import top.starrysea.object.dto.Question;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StarrtseaApplication.class)
+public class QuestionServiceTest {
+	@Autowired
+	private IQuestionService questionService;
+
+	@Test
+	public void queryAllQuestionServiceTest() {
+		Condition condition = new Condition();
+		condition.setPage(1);
+		System.out.println(questionService.queryAllQuestionService(condition,
+				new Question.Builder().questionStatus((short) 2).build()));
+	}
+
+	@Test
+	public void askQuestionServiceTest() {
+		System.out.println(
+				questionService.askQuestionService(new Question.Builder().question("zuzhangnvzhuang").build()));
+	}
+
+	@Test
+	public void answerQuestionServiceTest() {
+		System.out.println(questionService
+				.answerQuestionService(new Question.Builder().answer("haode").questionId("Q-0T2d4Wc1").build()));
+	}
+}
