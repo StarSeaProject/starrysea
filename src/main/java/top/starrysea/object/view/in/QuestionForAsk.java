3d3241c5ae2fad3809d5cf06c71bfc2849b69719
liuyang <liuyang@windows10.microdone.cn>
2018-01-25 22:16:49 +0800

    格式化代码

--- src/main/java/top/starrysea/object/view/in/QuestionForAsk.java
+++ src/main/java/top/starrysea/object/view/in/QuestionForAsk.java
@@ -1,14 +1,13 @@
 package top.starrysea.object.view.in;
 
-
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import top.starrysea.object.dto.Question;
 
 public class QuestionForAsk {
-	@NotEmpty(message="问题或意见不能为空")
-	@Length(max=150,message="质问长度不能超过150")
+	@NotEmpty(message = "问题或意见不能为空")
+	@Length(max = 150, message = "质问长度不能超过150")
 	private String question;
 
 	public String getQuestion() {
@@ -18,7 +17,8 @@ public class QuestionForAsk {
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	public Question toDTO(){
+
+	public Question toDTO() {
 		return new Question.Builder().question(question).build();
 	}
 }
