69b92a6392f1aa74718306f7de1fd7222a119188
liuyang <liuyang@windows10.microdone.cn>
2018-01-25 22:04:23 +0800

    添加queryAllController接口与queryAllControllerAjax接口

--- src/main/java/top/starrysea/controller/IQuestionController.java
+++ src/main/java/top/starrysea/controller/IQuestionController.java
@@ -2,12 +2,19 @@ package top.starrysea.controller;
 
 import java.util.Map;
 
+import org.springframework.mobile.device.Device;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
 
+import top.starrysea.common.Condition;
+import top.starrysea.object.view.in.QuestionForAll;
 import top.starrysea.object.view.in.QuestionForAnswer;
 import top.starrysea.object.view.in.QuestionForAsk;
 
 public interface IQuestionController {
+	ModelAndView queryQuestionController(Condition condition,QuestionForAll question, Device device);
+	
+	Map<String, Object> queryQuestionControllerAjax(QuestionForAll question);
 
 	Map<String, Object> askQuestionController(QuestionForAsk question, BindingResult bindingResult);
 
