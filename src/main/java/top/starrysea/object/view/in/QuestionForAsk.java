233a8467b6acf8b1d4c092924ceba0b7b6e7f476
liuyang <liuyang@windows10.microdone.cn>
2018-01-25 22:11:34 +0800

    补全了提出问题的入参属性与对应的DTO转换方法

--- src/main/java/top/starrysea/object/view/in/QuestionForAsk.java
+++ src/main/java/top/starrysea/object/view/in/QuestionForAsk.java
@@ -1,5 +1,24 @@
 package top.starrysea.object.view.in;
 
-public class QuestionForAsk {
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import top.starrysea.object.dto.Question;
+
+public class QuestionForAsk {
+	@NotEmpty(message="问题或意见不能为空")
+	@Length(max=150,message="质问长度不能超过150")
+	private String question;
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public Question toDTO(){
+		return new Question.Builder().question(question).build();
+	}
 }
