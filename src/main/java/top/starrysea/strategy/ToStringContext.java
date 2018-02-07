c6193377bc95f1140b7e71755ccd6e9033503cbd
liuyang <liuyang@windows10.microdone.cn>
2018-02-07 22:45:20 +0800

    增加对bindingResult、device、session的逻辑判断

--- src/main/java/top/starrysea/strategy/ToStringContext.java
+++ src/main/java/top/starrysea/strategy/ToStringContext.java
@@ -1,6 +1,10 @@
 package top.starrysea.strategy;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mobile.device.Device;
+import org.springframework.validation.BindingResult;
 
 import top.starrysea.common.Condition;
 import top.starrysea.object.view.in.OrderForAdd;
@@ -12,17 +16,23 @@ import top.starrysea.object.view.in.WorkForAll;
  */
 public class ToStringContext {
 	@Autowired
-	private ChangeToString changeToString ;
+	private ChangeToString changeToString;
 
 	public String toString(Object object) {
 		if (object instanceof OrderForAdd) {
 			changeToString = new OAddToString();
 		} else if (object instanceof WorkForAdd) {
 			changeToString = new WAddToString();
+		} else if (object instanceof HttpSession) {
+			changeToString = new SessionToString();
 		} else if (object instanceof Condition) {
 			changeToString = new ConditionToString();
 		} else if (object instanceof WorkForAll) {
 			changeToString = new WAllToString();
+		} else if (object instanceof BindingResult) {
+			changeToString = new BResultToString();
+		} else if (object instanceof Device) {
+			changeToString = new DeviceToString();
 		} else {
 			changeToString = new ObjectToString();
 		}
