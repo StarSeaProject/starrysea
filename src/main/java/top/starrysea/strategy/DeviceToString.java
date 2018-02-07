aad054e21b592c211302a1656275d9933073fce5
liuyang <liuyang@windows10.microdone.cn>
2018-02-07 22:43:00 +0800

    日志打印增加对Device的处理

---
+++ src/main/java/top/starrysea/strategy/DeviceToString.java
@@ -0,0 +1,21 @@
+package top.starrysea.strategy;
+
+import org.springframework.mobile.device.Device;
+
+public class DeviceToString implements ChangeToString {
+
+	@Override
+	public String paramToString(Object object) {
+		Device device = (Device) object;
+		String type = "UNKNOWN";
+		if (device.isNormal()) {
+			type = "电脑";
+		} else if (device.isMobile()) {
+			type = "手机";
+		} else if (device.isTablet()) {
+			type = "平板";
+		}
+		return device.getClass().getSimpleName() + "{\"deviceType\":" + type + "}";
+	}
+
+}
