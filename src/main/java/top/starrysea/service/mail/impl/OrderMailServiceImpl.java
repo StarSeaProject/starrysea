e4644311afff3181dc0927a8ccde146188afcbaa
xiongjunlang <xiongjunlang@agree.com.cn>
2018-02-08 11:38:38 +0800

    修复下单确认邮件中下单详细信息显示错误的bug

--- src/main/java/top/starrysea/service/mail/impl/OrderMailServiceImpl.java
+++ src/main/java/top/starrysea/service/mail/impl/OrderMailServiceImpl.java
@@ -35,9 +35,8 @@ public class OrderMailServiceImpl extends MailServiceImpl {
 		}
 		Orders order = orderDetails.get(0).getOrder();
 		StringBuilder orderDetailsStr = new StringBuilder(
-				"<table><thead><tr><td>作品名称</td><td>作品类型</td></tr></thead><tbody>");
-		for (Entity entity : entitys) {
-			OrderDetail orderDetail = (OrderDetail) entity;
+				"<table border=1><thead><tr><td>作品名称</td><td>作品类型</td></tr></thead><tbody>");
+		for (OrderDetail orderDetail : orderDetails) {
 			orderDetailsStr.append("<tr>");
 			orderDetailsStr.append("<td>" + orderDetail.getWorkType().getWork().getWorkName() + "</td>");
 			orderDetailsStr.append("<td>" + orderDetail.getWorkType().getName() + "</td>");
@@ -45,7 +44,7 @@ public class OrderMailServiceImpl extends MailServiceImpl {
 		}
 		orderDetailsStr.append("</tbody>");
 		orderDetailsStr.append("</table>");
-		String content = "<div>" + "这是一封确认邮件</div>" + "<div>您已经下单星之海的作品,以下是您下单的详细信息:</div>" + orderDetails.toString()
+		String content = "<div>" + "这是一封确认邮件</div>" + "<div>您已经下单星之海的作品,以下是您下单的详细信息:</div>" + orderDetailsStr.toString()
 				+ "<div>您的订单号为：<b>" + order.getOrderNum() + "</b></div>"
 				+ "<div>您可以根据此订单号<a href=http://www.starrysea.top/order/" + order.getOrderNum() + ">查询您的发货信息</a></div>"
 				+ "<div>我们会在发货后再发一封邮件来通知您</div>" + "<div>如有问题请联系我们的官博或者邮件联系adky8a@qq.com</div>";
