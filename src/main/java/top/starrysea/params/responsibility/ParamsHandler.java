fc5aac1a76ee9f5da1bbf875293aaa75670086f3
xiongjunlang <xiongjunlang@agree.com.cn>
2018-02-25 13:08:08 +0800

    删除strategy包

--- src/main/java/top/starrysea/params/responsibility/ParamsHandler.java
+++ src/main/java/top/starrysea/params/responsibility/ParamsHandler.java
@@ -1,13 +1,10 @@
 package top.starrysea.params.responsibility;
 
-import top.starrysea.params.strategy.ChangeToString;
-
 /*
  * 抽象职责者
  */
 public abstract class ParamsHandler {
 	protected ParamsHandler nextHandler;
-	protected ChangeToString changeToString;
 
 	public void setNextHandler(ParamsHandler nextHander) {
 		this.nextHandler = nextHander;
--- src/main/java/top/starrysea/params/strategy/ChangeToString.java
+++
@@ -1,8 +0,0 @@
-package top.starrysea.params.strategy;
-
-/*
- * 策略接口
- */
-public interface ChangeToString {
-	String paramToString(Object object);
-}
