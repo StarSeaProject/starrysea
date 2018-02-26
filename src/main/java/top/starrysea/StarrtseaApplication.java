d15bffa1be36a3946ac195e0e442d3f3e4eedff8
xiongjunlang <xiongjunlang@agree.com.cn>
2018-02-26 10:50:13 +0800

    kql使用@Bean方式注入

--- src/main/java/top/starrysea/StarrtseaApplication.java
+++ src/main/java/top/starrysea/StarrtseaApplication.java
@@ -3,6 +3,10 @@ package top.starrysea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import top.starrysea.kql.facede.KumaSqlDao;
+import top.starrysea.kql.facede.KumaSqlDaoImpl;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,5 +14,10 @@ public class StarrtseaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarrtseaApplication.class, args);
+	}
+
+	@Bean
+	public KumaSqlDao getKumaSqlDao() {
+		return new KumaSqlDaoImpl();
 	}
 }
