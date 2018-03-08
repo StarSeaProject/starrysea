32a2bdf79195161131dafa9c44b81337172bcf03
xiongjunlang <xiongjunlang@agree.com.cn>
2018-03-08 11:38:56 +0800

    增加下单的黑名单功能

---
+++ src/main/java/top/starrysea/aspect/OrderBlacklistAspect.java
@@ -0,0 +1,33 @@
+package top.starrysea.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import top.starrysea.common.ServiceResult;
+import top.starrysea.object.bo.Blacklist;
+import top.starrysea.object.dto.Orders;
+
+@Component
+@Aspect
+@EnableConfigurationProperties({Blacklist.class})
+public class OrderBlacklistAspect {
+
+	@Autowired
+	private Blacklist blacklist;
+
+	@Around("execution(* top.starrysea.service.impl.OrderServiceImpl.addOrderService(..))")
+	public Object isPeopleInBlacklist(ProceedingJoinPoint pjp) throws Throwable {
+		Orders order = (Orders) pjp.getArgs()[0];
+		if (blacklist.getEmailList().contains(order.getOrderEMail())
+				|| blacklist.getPhoneList().contains(order.getOrderPhone())) {
+			ServiceResult sr = new ServiceResult(false);
+			sr.setErrInfo("下单失败，请与管理员联系");
+			return sr;
+		}
+		return pjp.proceed();
+	}
+}
---
+++ src/main/java/top/starrysea/object/bo/Blacklist.java
@@ -0,0 +1,33 @@
+package top.starrysea.object.bo;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "blacklist")
+@PropertySource("classpath:blacklist.properties")
+public class Blacklist {
+
+	private List<String> emailList;
+	private List<String> phoneList;
+
+	public List<String> getEmailList() {
+		return emailList;
+	}
+
+	public void setEmailList(List<String> emailList) {
+		this.emailList = emailList;
+	}
+
+	public List<String> getPhoneList() {
+		return phoneList;
+	}
+
+	public void setPhoneList(List<String> phoneList) {
+		this.phoneList = phoneList;
+	}
+
+}
