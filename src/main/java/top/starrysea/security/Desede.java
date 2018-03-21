29b6f0a55be8182e7af2b306440c779ce9a46650
liuyang <liuyang@Lenovo-PC>
2018-03-21 19:09:22 +0800

    去除无用的抽象类

--- src/main/java/top/starrysea/security/Desede.java
+++ src/main/java/top/starrysea/security/Desede.java
@@ -1,7 +1,7 @@
 package top.starrysea.security;
 
+import static top.starrysea.common.Const.CHARSET;
 import java.security.Key;
-import java.util.Arrays;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKeyFactory;
@@ -11,28 +11,28 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import top.starrysea.common.Common;
+
 @Component("desede")
-public class Desede extends AbstractAlgorithm {
+public class Desede implements SecurityAlgorithm {
 
 	private Logger logger = Logger.getLogger(this.getClass());
-	private  static final String KEY="YeahTiger!NoTiger!iyaiyaiyaiyaYeahTiger!";
+	private static final String KEY = "YeahTiger!NoTiger!iyaiyaiyaiyaYeahTiger!";
 	// 加解密向量,不要改
-	private  static final String IV = "12345678";
-	
+	private static final String IV = "12345678";
+
 	@Override
 	public String encrypt(String info) {
-		try{
-		Key deskey = null;
-		DESedeKeySpec spec = new DESedeKeySpec(KEY.getBytes("UTF-8"));
-		SecretKeyFactory keyfactory = SecretKeyFactory
-				.getInstance("desede");
-		deskey = keyfactory.generateSecret(spec);
-		Cipher cipher = Cipher.getInstance("desede/CBC/PKCS5Padding");
-		IvParameterSpec ips = new IvParameterSpec(IV.getBytes("UTF-8"));
-		cipher.init(Cipher.ENCRYPT_MODE, deskey, ips);
-		byte[] encryptData=cipher.doFinal(info.getBytes("UTF-8"));
-		return Common.byte2base64(encryptData);
-		}catch (Exception e) {
+		try {
+			Key deskey = null;
+			DESedeKeySpec spec = new DESedeKeySpec(KEY.getBytes(CHARSET));
+			SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
+			deskey = keyfactory.generateSecret(spec);
+			Cipher cipher = Cipher.getInstance("desede/CBC/PKCS5Padding");
+			IvParameterSpec ips = new IvParameterSpec(IV.getBytes(CHARSET));
+			cipher.init(Cipher.ENCRYPT_MODE, deskey, ips);
+			byte[] encryptData = cipher.doFinal(info.getBytes(CHARSET));
+			return Common.byte2base64(encryptData);
+		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
 		return null;
@@ -42,19 +42,17 @@ public class Desede extends AbstractAlgorithm {
 	public String decrypt(String encryptInfo) {
 		try {
 			Key deskey = null;
-			DESedeKeySpec spec = new DESedeKeySpec(KEY.getBytes("UTF-8"));
-			SecretKeyFactory keyfactory = SecretKeyFactory
-					.getInstance("desede");
+			DESedeKeySpec spec = new DESedeKeySpec(KEY.getBytes(CHARSET));
+			SecretKeyFactory keyfactory = SecretKeyFactory.getInstance("desede");
 			deskey = keyfactory.generateSecret(spec);
 			Cipher cipher = Cipher.getInstance("desede/CBC/NoPadding");
-			IvParameterSpec ips = new IvParameterSpec(IV.getBytes("UTF-8"));
+			IvParameterSpec ips = new IvParameterSpec(IV.getBytes(CHARSET));
 			cipher.init(Cipher.DECRYPT_MODE, deskey, ips);
 			byte[] decryptData = cipher.doFinal(Common.base642byte(encryptInfo));
-			return new String(decryptData, "UTF-8").trim();
+			return new String(decryptData, CHARSET).trim();
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
 		return "";
 	}
 }
-
